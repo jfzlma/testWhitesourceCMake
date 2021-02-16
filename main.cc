d7c68202ff4153afa02aae7616c2429a6808a4e0
Jafsal Manjapully <jfzlma@gmail.com>
2021-02-16 18:14:55 +0530

    first commit

---
+++ main.cc
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+// for convenience
+using json = nlohmann::json;
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    json j2 = {
+  {"pi", 3.141},
+  {"happy", true},
+  {"name", "Niels"},
+  {"nothing", nullptr},
+  {"answer", {
+    {"everything", 42}
+  }},
+  {"list", {1, 0, 2}},
+  {"object", {
+    {"currency", "USD"},
+    {"value", 42.99}
+  }}
+};
+
+std::cout<< j2["name"] << std::endl;
+
+
+}
